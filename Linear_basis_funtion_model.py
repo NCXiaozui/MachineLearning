7bfc7958e9304b1a3fb71819b800d8b5e8fa2fa2
XYlander <xylander23@sohu.com>
2017-06-12 22:11:04 +0800

    finish regularized logic kernel linear regression

--- Linear_basis_funtion_model.py
+++ Linear_basis_funtion_model.py
@@ -4,7 +4,8 @@
   The code of Linear Basis Function Models 
 '''
 
-# -*- coding: utf-8 -*-
+#train_set mean of every features: 0:0.0294638316759 1:0.0636844215484 2:-0.0245251016585 3:-0.023323761252 4:-0.0178207686246 5:-0.0311765153256 6:-0.0076677187343 7:0.014989786312 8:0.00106815786274 9:-0.00656270921372
+#regression_set mean of every feature: -0.0294638316759 1:-0.0636844215484 2:0.0245251016585 3:0.023323761252 4:0.0178207686246 5:0.0311765153256 6:0.00766771873428 7:-0.014989786312 8:-0.00106815786275 9:0.0065627092137
 '''
 The code is to more deeply understand the PRML Chapter 3 about LinearRegression
 The data is diabetes patient set from sklearn
@@ -57,26 +58,26 @@ def LinearRegression(dt,dp,tl,pl,kernelfunction):
         for data in dp:
             tem = []
             for i in range(len(data)):
-                if i == 0:
-                    tem.append(np.exp(-(data[i] - 0.0294638316759) ** 2 / (2 * 0.11170534249)))
-                if i == 1:
-                    tem.append(np.exp(-(data[i] - 0.0636844215484) ** 2 / (2 * 0.107140579897)))
+                if i == 0 :
+                    tem.append(np.exp(-(data[i] - 0.0294638316759)**2 / (2*0.11170534249)))
+                if i == 1 :
+                    tem.append(np.exp(-(data[i] - 0.0636844215484)**2 / (2*0.107140579897)))
                 if i == 2:
-                    tem.append(np.exp(-(data[i] + 0.0245251016585) ** 2 / (2 * 0.116465058849)))
+                    tem.append(np.exp(-(data[i] + 0.0245251016585) ** 2 / (2*0.116465058849)))
                 if i == 3:
-                    tem.append(np.exp(-(data[i] + 0.023323761252) ** 2 / (2 * 0.1101685293)))
+                    tem.append(np.exp(-(data[i] + 0.023323761252) ** 2 / (2*0.1101685293)))
                 if i == 4:
-                    tem.append(np.exp(-(data[i] + 0.0178207686246) ** 2 / (2 * 0.110054133791)))
+                    tem.append(np.exp(-(data[i] + 0.0178207686246) ** 2 / (2*0.110054133791)))
                 if i == 5:
-                    tem.append(np.exp(-(data[i] + 0.0311765153256) ** 2 / (2 * 0.110768000707)))
+                    tem.append(np.exp(-(data[i] + 0.0311765153256) ** 2 / (2*0.110768000707)))
                 if i == 6:
-                    tem.append(np.exp(-(data[i] + 0.0076677187343) ** 2 / (2 * 0.109714460455)))
+                    tem.append(np.exp(-(data[i] + 0.0076677187343) ** 2 / (2*0.109714460455)))
                 if i == 7:
-                    tem.append(np.exp(-(data[i] - 0.014989786312) ** 2 / (2 * 0.111275377915)))
+                    tem.append(np.exp(-(data[i] - 0.014989786312) ** 2 / (2*0.111275377915)))
                 if i == 8:
-                    tem.append(np.exp(-(data[i] - 0.00106815786274) ** 2 / (2 * 0.115277205067)))
+                    tem.append(np.exp(-(data[i] - 0.00106815786274)**2 / (2*0.115277205067)))
                 if i == 9:
-                    tem.append(np.exp(-(data[i] + 0.00656270921372) ** 2 / (2 * 0.116974959819)))
+                    tem.append(np.exp(-(data[i] + 0.00656270921372)** 2 / (2*0.116974959819)))
 
             data_matrix.append(tem)
         data_matrix = np.mat(data_matrix)
@@ -111,6 +112,7 @@ def LinearRegression(dt,dp,tl,pl,kernelfunction):
         design_matrix = np.mat(design_matrix)
         t = np.mat(tl[:,np.newaxis])
         w = (design_matrix.T*design_matrix).I * design_matrix.T * t
+        print w
 
         for data in dp:
             tem = []
@@ -144,14 +146,6 @@ def LinearRegression(dt,dp,tl,pl,kernelfunction):
     return result
 
 
-
-
-
-
-
-
-
-
 def predictscore(pl,result):
     count = 0.0
     for i in range(len(pl)):
@@ -166,19 +160,141 @@ def predictscore(pl,result):
     score = 1 - sum_t/sum_mean
     print 'The score is {0}'.format(score)
 
+def regularizedlearnregression(dt,dp,tl,pl,kernelfunction):
+    lamb = 0.08
+    design_matrix = []
+    data_matrix = []
+    if kernelfunction == 'gauss':  # supposing standard gauss distribution
+        for data in dt:
+            tem = []
+            for i in range(len(data)):
+                if i == 0:
+                    tem.append(np.exp(-(data[i] - 0.0294638316759) ** 2 / (2 * 0.11170534249)))
+                if i == 1:
+                    tem.append(np.exp(-(data[i] - 0.0636844215484) ** 2 / (2 * 0.107140579897)))
+                if i == 2:
+                    tem.append(np.exp(-(data[i] + 0.0245251016585) ** 2 / (2 * 0.116465058849)))
+                if i == 3:
+                    tem.append(np.exp(-(data[i] + 0.023323761252) ** 2 / (2 * 0.1101685293)))
+                if i == 4:
+                    tem.append(np.exp(-(data[i] + 0.0178207686246) ** 2 / (2 * 0.110054133791)))
+                if i == 5:
+                    tem.append(np.exp(-(data[i] + 0.0311765153256) ** 2 / (2 * 0.110768000707)))
+                if i == 6:
+                    tem.append(np.exp(-(data[i] + 0.0076677187343) ** 2 / (2 * 0.109714460455)))
+                if i == 7:
+                    tem.append(np.exp(-(data[i] - 0.014989786312) ** 2 / (2 * 0.111275377915)))
+                if i == 8:
+                    tem.append(np.exp(-(data[i] - 0.00106815786274) ** 2 / (2 * 0.115277205067)))
+                if i == 9:
+                    tem.append(np.exp(-(data[i] + 0.00656270921372) ** 2 / (2 * 0.116974959819)))
+
+            design_matrix.append(tem)
+        design_matrix = np.mat(design_matrix)
+        t = np.mat(tl[:, np.newaxis])
+        w = (lamb *np.identity(10) + design_matrix.T * design_matrix).I * design_matrix.T * t
+
+        for data in dp:
+            tem = []
+            for i in range(len(data)):
+                if i == 0:
+                    tem.append(np.exp(-(data[i] - 0.0294638316759) ** 2 / (2 * 0.11170534249)))
+                if i == 1:
+                    tem.append(np.exp(-(data[i] - 0.0636844215484) ** 2 / (2 * 0.107140579897)))
+                if i == 2:
+                    tem.append(np.exp(-(data[i] + 0.0245251016585) ** 2 / (2 * 0.116465058849)))
+                if i == 3:
+                    tem.append(np.exp(-(data[i] + 0.023323761252) ** 2 / (2 * 0.1101685293)))
+                if i == 4:
+                    tem.append(np.exp(-(data[i] + 0.0178207686246) ** 2 / (2 * 0.110054133791)))
+                if i == 5:
+                    tem.append(np.exp(-(data[i] + 0.0311765153256) ** 2 / (2 * 0.110768000707)))
+                if i == 6:
+                    tem.append(np.exp(-(data[i] + 0.0076677187343) ** 2 / (2 * 0.109714460455)))
+                if i == 7:
+                    tem.append(np.exp(-(data[i] - 0.014989786312) ** 2 / (2 * 0.111275377915)))
+                if i == 8:
+                    tem.append(np.exp(-(data[i] - 0.00106815786274) ** 2 / (2 * 0.115277205067)))
+                if i == 9:
+                    tem.append(np.exp(-(data[i] + 0.00656270921372) ** 2 / (2 * 0.116974959819)))
+
+            data_matrix.append(tem)
+        data_matrix = np.mat(data_matrix)
+        result = w.T * data_matrix.T
+
+    if kernelfunction == 'logic':
+        for data in dt:
+            tem = []
+            for i in range(len(data)):
+                if i == 0:
+                    tem.append(1.0 / (1.0 + np.exp(-(data[i] - 0.0294638316759) / (np.sqrt(0.11170534249)))))
+                if i == 1:
+                    tem.append(1.0 / (1.0 + np.exp(-(data[i] - 0.0636844215484) / (np.sqrt(0.107140579897)))))
+                if i == 2:
+                    tem.append(1.0 / (1.0 + np.exp(-(data[i] + 0.0245251016585) / (np.sqrt(0.116465058849)))))
+                if i == 3:
+                    tem.append(1.0 / (1.0 + np.exp(-(data[i] + 0.023323761252) / (np.sqrt(0.1101685293)))))
+                if i == 4:
+                    tem.append(1.0 / (1.0 + np.exp(-(data[i] + 0.0178207686246) / (np.sqrt(0.110054133791)))))
+                if i == 5:
+                    tem.append(1.0 / (1.0 + np.exp(-(data[i] + 0.0311765153256) / (np.sqrt(0.110768000707)))))
+                if i == 6:
+                    tem.append(1.0 / (1.0 + np.exp(-(data[i] + 0.0076677187343) / (np.sqrt(0.109714460455)))))
+                if i == 7:
+                    tem.append(1.0 / (1.0 + np.exp(-(data[i] - 0.014989786312) / (np.sqrt(0.111275377915)))))
+                if i == 8:
+                    tem.append(1.0 / (1.0 + np.exp(-(data[i] - 0.00106815786274) / (np.sqrt(0.115277205067)))))
+                if i == 9:
+                    tem.append(1.0 / (1.0 + np.exp(-(data[i] + 0.00656270921372) / (np.sqrt(0.116974959819)))))
+
+            design_matrix.append(tem)
+        design_matrix = np.mat(design_matrix)
+        t = np.mat(tl[:, np.newaxis])
+        w = (lamb *np.identity(10) + design_matrix.T * design_matrix).I * design_matrix.T * t
+        print w
+        for data in dp:
+            tem = []
+            for i in range(len(data)):
+                if i == 0:
+                    tem.append(1.0 / (1.0 + np.exp(-(data[i] - 0.0294638316759) / (np.sqrt(0.11170534249)))))
+                if i == 1:
+                    tem.append(1.0 / (1.0 + np.exp(-(data[i] - 0.0636844215484) / (np.sqrt(0.107140579897)))))
+                if i == 2:
+                    tem.append(1.0 / (1.0 + np.exp(-(data[i] + 0.0245251016585) / (np.sqrt(0.116465058849)))))
+                if i == 3:
+                    tem.append(1.0 / (1.0 + np.exp(-(data[i] + 0.023323761252) / (np.sqrt(0.1101685293)))))
+                if i == 4:
+                    tem.append(1.0 / (1.0 + np.exp(-(data[i] + 0.0178207686246) / (np.sqrt(0.110054133791)))))
+                if i == 5:
+                    tem.append(1.0 / (1.0 + np.exp(-(data[i] + 0.0311765153256) / (np.sqrt(0.110768000707)))))
+                if i == 6:
+                    tem.append(1.0 / (1.0 + np.exp(-(data[i] + 0.0076677187343) / (np.sqrt(0.109714460455)))))
+                if i == 7:
+                    tem.append(1.0 / (1.0 + np.exp(-(data[i] - 0.014989786312) / (np.sqrt(0.111275377915)))))
+                if i == 8:
+                    tem.append(1.0 / (1.0 + np.exp(-(data[i] - 0.00106815786274) / (np.sqrt(0.115277205067)))))
+                if i == 9:
+                    tem.append(1.0 / (1.0 + np.exp(-(data[i] + 0.00656270921372) / (np.sqrt(0.116974959819)))))
+
+            data_matrix.append(tem)
+        data_matrix = np.mat(data_matrix)
+        result = w.T * data_matrix.T
+    return result
+
 def stastic(dt):
     count = 0.0
     for data in dt:
         for i in range(len(data)):
             if i == 9:
-                count += (data[i]- 0.0294638316759)**2 #0:0.0294638316759 1:0.0636844215484 2:-0.0245251016585 3:-0.023323761252 4:-0.0178207686246 5:-0.0311765153256 6:-0.0076677187343 7:0.014989786312 8:0.00106815786274 9:-0.00656270921372
-
+                count += (data[i])
     print count/len(data)
 
 
 if __name__=='__main__':
     dt,dp,tl,pl=load_data()
     result = LinearRegression(dt,dp,tl,pl,'logic')
-    #stastic(dt)
+    result = regularizedlearnregression(dt,dp,tl,pl,'logic')
+    predictscore(pl, result)
+    #stastic(dp)
     #print np.mat(tl[:,np.newaxis]).shape
-    predictscore(pl,result)
+
